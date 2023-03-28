95356772f097dac09700f7a7e3234c83aefa85ab
ahmadkismael <ahmad.abualkhair@gmail.com>
2023-03-28 14:45:35 -0600

    second lab day

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
+from .models import Car
 
 # Create your views here.
 
-cars = [
-    {'make': 'Nissan', 'model': 'GTR', 'year': '2023', 'description': 'Fastest car created by Nissan'},
-    {'make': 'BMW', 'model': 'm6', 'year': '2023', 'description': 'Fastest car created by BMW'},
-    {'make': 'Audi', 'model': 'R8', 'year': '2023', 'description': 'Fastest car created by Audi'},
-    {'make': 'Ford', 'model': 'Mustang Shelby500', 'year': '2023', 'description': 'One of the fastest cars created by Ford'},
-]
+
 
 def home(request):
     return render(request, 'home.html')
@@ -18,6 +14,14 @@ def about(request):
 
 
 def cars_index(request):
+    cars = Car.objects.all()
     return render(request, 'cars/index.html', {
         'cars': cars
     })
+
+def cars_detail(request, car_id):
+    car = Car.objects.get(id=car_id)
+    return render(request, 'cars/detail.html', {
+        'car': car
+    })
+
